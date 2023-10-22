79534a5808c4d8f11060ab0185f1dec97cc5259c
Thomas4891 <104730615+Thomas4891@users.noreply.github.com>
2023-10-22 18:37:13 -0400

    Edit code repeatString.js/repeatString.spec.js
    Remove .skip from repeatString.spec.js.
    Add conditionals to pass extra tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,15 @@
-const repeatString = function (string, num) {
-    let concatString = string;
+const repeatString = function(string, num) {
+  let concatString = string;
+  if (num === 0) {
+    return '';
+  } else if (num < 0) {
+    return 'ERROR';
+  } else {
     for (let i = 1; i < num; ++i) {
-        concatString += string;
+      concatString += string;
     }
     return concatString;
+  }
 };
 
 // Do not edit below this line
