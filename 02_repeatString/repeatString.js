8d13f9c22bf9d2babe9e6dd905c89a5378135dc3
Thomas4891 <104730615+Thomas4891@users.noreply.github.com>
2023-10-22 18:34:21 -0400

    Add code to repeatString to pass first test.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let concatString = string;
+    for (let i = 1; i < num; ++i) {
+        concatString += string;
+    }
+    return concatString;
 };
 
 // Do not edit below this line
