8e9c8fb56b074c4d2141aa4fa96e2fa5d0b4cc31
Thomas4891 <104730615+Thomas4891@users.noreply.github.com>
2023-10-22 19:33:33 -0400

    Add code to pass 1st test in reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let revStr = [];
+  for (let i = string.length - 1; i >= 0; --i) {
+    revStr.push(string[i]);
+  }
+  return revStr.join('');
 };
-
 // Do not edit below this line
 module.exports = reverseString;
