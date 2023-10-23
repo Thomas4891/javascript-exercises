8dfc22d0288e59f211acfcbfb9a7f17023ff48d4
Thomas4891 <104730615+Thomas4891@users.noreply.github.com>
2023-10-23 19:54:35 -0400

    Add code to removeFromArray() to pass tests
    Removed .skip in removeFromArray.spec.js. Added code to removeFromArray
    so it takes array as first argument and one or more arguments to remove
    from the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...element) {
+  for (let i = 0; i < element.length; ++i) {
+    for (let j = 0; j < array.length; ++j) {
+      if (element[i] === array[j]) {
+        array.splice(j, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
