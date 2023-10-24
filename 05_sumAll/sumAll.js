63760d57257c96bca05f173326563e257edb1930
Thomas4891 <104730615+Thomas4891@users.noreply.github.com>
2023-10-23 20:44:33 -0400

    Add code to sumAll function to pass 1st test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 > num2) {
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+  let sumAll = 0;
 
+  for (let i = 1; i <= num2; ++i) {
+    sumAll += i;
+  }
+  return sumAll;
 };
 
 // Do not edit below this line
