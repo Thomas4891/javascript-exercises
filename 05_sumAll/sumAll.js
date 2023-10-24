f6f8807ce12bba7691dd058e06923d0f1cf83e74
Thomas4891 <104730615+Thomas4891@users.noreply.github.com>
2023-10-23 21:16:17 -0400

    Add code to pass all tests in sumAll.spec.js
    Adds conditionals to check if arguments are a number, not negative, and
    check if bigger number is first argument.
    Removed .skip from tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,19 @@
 const sumAll = function(num1, num2) {
-  if (num1 > num2) {
-    let temp = num1;
-    num1 = num2;
-    num2 = temp;
-  }
   let sumAll = 0;
-
-  for (let i = 1; i <= num2; ++i) {
-    sumAll += i;
+  if (typeof num1 === 'number' && typeof num2 === 'number' && num1 > 0 &&
+      num2 > 0) {
+    if (num1 > num2) {
+      let temp = num1;
+      num1 = num2;
+      num2 = temp;
+    }
+    for (let i = 1; i <= num2; ++i) {
+      sumAll += i;
+    }
+    return sumAll;
+  } else {
+    return 'ERROR';
   }
-  return sumAll;
 };
 
 // Do not edit below this line
